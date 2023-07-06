95e1a1bf4e9698cf4987bf0aed8fbb8e1ec94051
Karina <103330695+karinaKarinakarinaKarina@users.noreply.github.com>
2023-07-06 20:31:03 +0300

    Delete target directory

--- target/debug/deps/tasks-5b6ef2f4d61b8dcd.d
+++
@@ -1,5 +0,0 @@
-/home/zbykovd/karina/tasks/target/debug/deps/tasks-5b6ef2f4d61b8dcd.rmeta: src/main.rs
-
-/home/zbykovd/karina/tasks/target/debug/deps/tasks-5b6ef2f4d61b8dcd.d: src/main.rs
-
-src/main.rs:
