c8b5a24b1c8082dd79a69802eefae0d63dc4ae66
Karina <103330695+karinaKarinakarinaKarina@users.noreply.github.com>
2023-07-06 20:42:12 +0300

    add  task1.rs

--- second_homework/src/task1.rs
+++ second_homework/src/task1.rs
@@ -7,31 +7,41 @@
  */
 
 const SEARCH_TERM: &str = "picture";
-const QUOTE: &str = "Every face, every shop, bedroom window, public-house, and
- dark square is a picture feverishly turned--in search of what?
- It is the same with books. What do we seek through millions of pages?";
+const QUOTE: &str = 
+"Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books. What do we seek through millions of pages?";
+
 
 fn main() {
+    // for line in QUOTE.lines(){
+    //     println!("{}", line);
+    //     println!("q");
+    // };
+
     find_term(SEARCH_TERM, QUOTE);
 }
 
-fn find_term(search_term: &str, quote: &str) -> () {
-    let lines = quote.lines();
-    let mut number_of_line = 1;
-    for line in lines {
+fn find_term(search_term: &str, quote: &str) -> String {
+    let mut num_line = 1;
+
+    for line in quote.lines() {
         if line.contains(search_term) {
-            println!("номер строки: {}", number_of_line);
-            println!("{}", line.to_string());
-        }
-        number_of_line += 1;
+            // return num_line.to_string().push_str(": ").push_str(line)
+            return format!("{}: {}", num_line, line.trim());
+
+        };
+        num_line += 1
     }
+    "".to_string()
 }
+
 
 // ----> TESTS
 #[cfg(test)]
 mod tests {
     use crate::find_term;
-    use crate::{QUOTE, SEARCH_TERM};
+    use crate::{SEARCH_TERM, QUOTE};
 
     #[test]
     fn correct_line() {
