40fc5236a2c3d0ee466f489eb625c133e103d8c7
Karina <103330695+karinaKarinakarinaKarina@users.noreply.github.com>
2023-07-06 20:28:43 +0300

    Delete second_homework/task1 directory

--- second_homework/task1/target/debug/deps/task1-fa8108b67ef10cb8.d
+++
@@ -1,5 +0,0 @@
-/home/zbykovd/karina/tasks/target/debug/deps/task1-fa8108b67ef10cb8.rmeta: src/bin/task1.rs
-
-/home/zbykovd/karina/tasks/target/debug/deps/task1-fa8108b67ef10cb8.d: src/bin/task1.rs
-
-src/bin/task1.rs:
